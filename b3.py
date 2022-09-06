1b44b67c6481ee0153a1d007ce32ae0155b7e26d
Rekiichan <dtnhan.ete.dut@gmail.com>
2022-09-06 11:57:54 +0700

    b3_update2

--- b3.py
+++ b3.py
@@ -1,6 +1,5 @@
 import cv2 as cv
 import numpy as np
-import matplotlib as mpl 
 from matplotlib import pyplot as plt
 
 Init_matrix = cv.imread('2_2.bmp')
@@ -12,18 +11,8 @@ for r in range(0,rows):
     for c in range(0,cols):
         freq_matrix[crop_matrix[r][c]] += 1
 
-max = freq_matrix[0]
-for i in range(1,256):
-    if freq_matrix[i] > max:
-        max = freq_matrix[i]
-
-fig,ax = plt.subplots(1,1)
-x_axis = np.linspace(0,255,20)
-ax.hist(freq_matrix, bins = x_axis)
-ax.set_title("Histogram")
-ax.set_ylabel('freq')
-ax.set_xlabel('value')
+axis_X = np.arange(0, 256, 1)
+plt.bar(axis_X,freq_matrix)
+plt.ylabel("value")
 plt.show()
 # np.savetxt('result.txt',freq_matrix)
-
-
