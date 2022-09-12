191582e8f9663de2f5e703f1ba008d467face062
Rekiichan <dtnhan.ete.dut@gmail.com>
2022-09-12 22:34:11 +0700

    updateBai3

--- b2.py
+++ b2.py
@@ -5,17 +5,17 @@ from matplotlib import pyplot as plt
 Init_matrix = cv.imread('2_2.bmp')
 float_matrix = np.array(Init_matrix).astype(float)
 
-LSB = float_matrix % 2
-floor_1st = np.floor(float_matrix / 2) % 2
-floor_2nd = np.floor(float_matrix / 4) % 2
-floor_3rd = np.floor(float_matrix / 8) % 2
-floor_4th = np.floor(float_matrix / 16) % 2
-floor_5th = np.floor(float_matrix / 32) % 2
-floor_6th = np.floor(float_matrix / 64) % 2
-MSB = np.floor(float_matrix / 128) % 2
+floor_1 = float_matrix % 2
+floor_2 = np.floor(float_matrix / 2) % 2
+floor_3 = np.floor(float_matrix / 4) % 2
+floor_4 = np.floor(float_matrix / 8) % 2
+floor_5 = np.floor(float_matrix / 16) % 2
+floor_6 = np.floor(float_matrix / 32) % 2
+floor_7 = np.floor(float_matrix / 64) % 2
+floor_8 = np.floor(float_matrix / 128) % 2
 
-Rec_Image = (2 * (2 * (2 * (2 * (2 * (2 * (2 * MSB + floor_6th) + floor_5th)
-+ floor_4th) + floor_3rd) + floor_3rd) + floor_2nd) + LSB)
+Rec_Image = (2 * (2 * (2 * (2 * (2 * (2 * (2 * floor_8 + floor_7) + floor_6)
++ floor_5) + floor_4) + floor_4) + floor_3) + floor_1)
 
 fig = plt.figure(figsize=(12, 9))
 
@@ -25,42 +25,42 @@ plt.title('Original Image')
 plt.axis("off")
   
 fig.add_subplot(2, 5, 2)
-plt.imshow(LSB)
+plt.imshow(floor_1)
 plt.title('Bit Plane 1')
 plt.axis("off")
 
 fig.add_subplot(2, 5, 3)
-plt.imshow(floor_1st)
+plt.imshow(floor_2)
 plt.title('Bit Plane 2')
 plt.axis("off")
 
 fig.add_subplot(2, 5, 4)
-plt.imshow(floor_2nd)
+plt.imshow(floor_3)
 plt.title('Bit Plane 3')
 plt.axis("off")
 
 fig.add_subplot(2, 5, 5)
-plt.imshow(floor_3rd)
+plt.imshow(floor_4)
 plt.title('Bit Plane 4')
 plt.axis("off")
 
 fig.add_subplot(2, 5, 6)
-plt.imshow(floor_4th)
+plt.imshow(floor_5)
 plt.title('Bit Plane 5')
 plt.axis("off")
 
 fig.add_subplot(2, 5, 7)
-plt.imshow(floor_5th)
+plt.imshow(floor_6)
 plt.title('Bit Plane 6')
 plt.axis("off")
 
 fig.add_subplot(2, 5, 8)
-plt.imshow(floor_6th)
+plt.imshow(floor_7)
 plt.title('Bit Plane 7')
 plt.axis("off")
 
 fig.add_subplot(2, 5, 9)
-plt.imshow(MSB)
+plt.imshow(floor_8)
 plt.title('Bit Plane 8')
 plt.axis("off")
 
--- b3.py
+++ b3.py
@@ -2,17 +2,38 @@ import cv2 as cv
 import numpy as np
 from matplotlib import pyplot as plt
 
-Init_matrix = cv.imread('2_2.bmp')
-crop_matrix = Init_matrix[0:512,0:512,0]
-rows, cols = crop_matrix.shape
+init_matrix = cv.imread('2_2.bmp')
+cropped_matrix = init_matrix[0:512,0:512,0]
+rows, cols = cropped_matrix.shape
 freq_matrix = np.zeros(256).astype(int)
 
 for r in range(0,rows):
     for c in range(0,cols):
-        freq_matrix[crop_matrix[r][c]] += 1
+        freq_matrix[cropped_matrix[r][c]] += 1
+sum = 0
+for r in range(0,rows):
+    for c in range(0,cols):
+        sum += 1
+
+# print(sum)
 
 axis_X = np.arange(0, 256, 1)
+
+plt.plot(freq_matrix)
 plt.bar(axis_X,freq_matrix)
 plt.ylabel("value")
+plt.show()
+
+p = freq_matrix / (rows*cols)
+cdf = np.zeros((256))
+for i in range (len(cdf)):
+    cdf[i] = np.sum(p[:i])              
+
+plt.plot(cdf)
+plt.title('The cumulative distribution function (cdf)')
+plt.bar(axis_X,cdf)
+plt.xlabel('Pixel Value')
+plt.ylabel('Probability')
+plt.title('Nomalized Histogram')
 plt.show()
 # np.savetxt('result.txt',freq_matrix)
