9dfb2f32781d83b75bbe45dc9e1a9ca41f548c4c
Rekiichan <dtnhan.ete.dut@gmail.com>
2022-09-13 12:51:05 +0700

    update bai 4b lan 2

--- b4_b.py
+++ b4_b.py
@@ -11,10 +11,12 @@ def get_histogram(src):
     for r in range(gray_img.shape[0]):
         for c in range(gray_img.shape[1]):
             freq[gray_img[r][c]] += 1
-    return freq,gray_img
+    return freq
 
 def hist_equal(src):
-    freq,gray_img = get_histogram(src)
+    hsv_img = cv.cvtColor(src,cv.COLOR_BGR2HSV)
+    h,s,gray_img = hsv_img[:,:,0], hsv_img[:,:,1], hsv_img[:,:,2]
+    freq = get_histogram(src)
     rows,cols = gray_img.shape
     size = rows*cols
     p = freq / size
@@ -27,13 +29,44 @@ def hist_equal(src):
     for row in range(gray_img.shape[0]): # traverse by row (y-axis)
         for col in range(gray_img.shape[1]): # traverse by column (x-axis)
             gray_img[row, col] = hist[gray_img[row, col]]
-    hsv_img = cv.cvtColor(src,cv.COLOR_BGR2HSV)
-    h,s = hsv_img[:,:,0], hsv_img[:,:,1]
+
     hsv_img = cv.merge([h,s,gray_img])
     bgr_img = cv.cvtColor(hsv_img,cv.COLOR_HSV2BGR)
     rgb_cvt_img = cv.cvtColor(bgr_img,cv.COLOR_BGR2RGB)
     return rgb_cvt_img
 
+def display_img(original,converted_img):
+    hist_ori = get_histogram(original)
+    hist_cvt = get_histogram(converted_img)
+    axis_X = np.arange(0, 256, 1)
+    fig = plt.figure(figsize=(11, 7))
+    
+    fig.add_subplot(2, 2, 1)
+    plt.imshow(original)
+    plt.title('Original Image')
+    plt.axis("off")
+
+    fig.add_subplot(2, 2, 2)
+    plt.bar(axis_X,hist_ori)
+    plt.title('Histogram')
+
+    fig.add_subplot(2, 2, 3)
+    plt.imshow(converted_img)
+    plt.title('Converted Image')
+    plt.axis("off")
+
+    fig.add_subplot(2, 2, 4)
+    plt.bar(axis_X,hist_cvt)
+    plt.title('Histogram')
+
+    plt.show()
+
 if __name__ == '__main__':
     img_2_1 = cv.imread('2_1.jpg')
+    rgb_src_img = cv.cvtColor(img_2_1,cv.COLOR_BGR2RGB)
     rgb_cvt_img = hist_equal(img_2_1)
+    display_img(rgb_src_img,rgb_cvt_img)
+    # img_2_3 = cv.imread('2_3.jpg')
+    # rgb_src_img = cv.cvtColor(img_2_3,cv.COLOR_BGR2RGB)
+    # rgb_cvt_img = hist_equal(img_2_3)
+    # display_img(rgb_src_img,rgb_cvt_img)
